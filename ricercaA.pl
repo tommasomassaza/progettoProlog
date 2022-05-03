4cabe42dc3b4bb91a7ee89edef6f3ed1fb5872d4
tommasomassaza <104672150+tommasomassaza@users.noreply.github.com>
2022-05-03 22:56:04 +0200

    Update ricercaA.pl

--- ricercaA.pl
+++ ricercaA.pl
@@ -2,27 +2,20 @@ cerca_soluzioneA(ListaAzioni):-
     iniziale(SIniziale),
     %euristicaFuoriPosto(SIniziale,H),
     euristicaManhattan(SIniziale, H),
-    write(H),write('\n'),
-    aStar(ListaAzioni,[H,SIniziale,[],0],[],[],H),
+    aStar(ListaAzioni,[H,SIniziale,[],0],[],[]),
     !.
 
-aStar(ListaAzioni,[_,S,ListaAzioni,_],_,_,_):-
+aStar(ListaAzioni,[_,S,ListaAzioni,_],_,_):-
     finale(S),
     !.
-/*aStar(ListaAzioni,[F,S,PercorsoPerS,Costo],Successori,NodiEspansi,MinEur):-
-    H is F - Costo,
-    H < MinEur,
-    write(H),write('\n'),
-    aStar(ListaAzioni,[F,S,PercorsoPerS,Costo],Successori,NodiEspansi,H),
-    !.*/
-aStar(ListaAzioni,[_,S,PercorsoPerS,Costo],Successori,NodiEspansi,MinEur):-
+aStar(ListaAzioni,[_,S,PercorsoPerS,Costo],Successori,NodiEspansi):-
     findall(Az,applicabile(Az,S),AzioniApplicabili),
     NuovoCosto is Costo + 1,
     espansione(S,AzioniApplicabili,PercorsoPerS,NuovoCosto,NodiFigli,NodiEspansi),
     append(Successori,NodiFigli,NuoviSuccessori),
     msort(NuoviSuccessori,[NodoDaEspandere|NuoviSuccessoriOrdinati]), % ordina in maniera crescente in base a F
     deletePers(NuoviSuccessoriOrdinati,NodoDaEspandere,NuoviSuccessoriOrdinati_), % rimuove tutti i nodi con lo stesso stato del primo nella lista che verrà espanso
-    aStar(ListaAzioni,NodoDaEspandere,NuoviSuccessoriOrdinati_,[S|NodiEspansi],MinEur).
+    aStar(ListaAzioni,NodoDaEspandere,NuoviSuccessoriOrdinati_,[S|NodiEspansi]).
 
 espansione(_,[],_,_,[],_).
 espansione(S,[Az|AzioniRestanti],PercorsoPerS,Costo,[[F,S_Nuovo,PercorsoPerS_Nuovo,Costo]|NodiFigli],NodiEspansi):-
